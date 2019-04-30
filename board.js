478c1a0467c7dbc5f3bc80ea429420e27103f8ed
GamerJpop <46901016+GamerJpop@users.noreply.github.com>
2019-04-30 10:27:51 -0400

    Game Done!

---
+++ board.js
@@ -0,0 +1,100 @@
+$(document).ready(function () {
+    console.log('document ready')
+    $('#board-container').html(renderBoard())
+    renderCheckers()
+})
+
+
+
+function renderBoard() {
+    return `    
+        ${renderRow(1)}
+        ${renderRow(2)}
+        ${renderRow(3)}
+        ${renderRow(4)}
+        ${renderRow(5)}
+        ${renderRow(6)}
+        ${renderRow(7)}
+        ${renderRow(8)}
+
+` 
+}
+
+
+function renderRow(rowNum) {
+    return `
+        <div id="row-${rowNum}" class="row">
+            ${renderCell(rowNum, 1)}
+            ${renderCell(rowNum, 2)}
+            ${renderCell(rowNum, 3)}
+            ${renderCell(rowNum, 4)}
+            ${renderCell(rowNum, 5)}
+            ${renderCell(rowNum, 6)}
+            ${renderCell(rowNum, 7)}
+            ${renderCell(rowNum, 8)}
+        </div>
+    `
+}
+
+function renderCell(rowNum, cellNum) {
+    if (cellColor(cellNum, rowNum) === 'black') {
+        // Black Cell
+        return `<div id="cell-${rowNum}-${cellNum}" class="cell black"></div>` 
+    } else 
+    // White Cell
+        return `<div id="cell-${rowNum}-${cellNum}" class="cell red"></div>`
+}
+
+
+/*** Helper Methods ***/
+function parity(num) {
+    return (num % 2 == 0) ? 'even' : 'odd'
+}
+
+function cellColor(cellNum, rowNum) {
+    return parity(cellNum) == parity(rowNum) ? 'red' : 'black'
+}
+
+
+function moveSelectedCheckerHere() {
+    console.log('things')
+    if(selectedChecker) {
+        console.log(`move checker here`)
+        
+        let blackCell = $(this)
+        console.log(`blackCell`, blackCell) 
+        
+        let id = blackCell.attr('id')
+        console.log(`id:`, id)
+        
+        
+        let idParts = id.split('-')
+        console.log(`idParts = `, idParts)
+        
+        selectedChecker.row = idParts[1]
+        selectedChecker.cell = idParts[2]
+        
+        if(selectedChecker.color == `black` && selectedChecker.row == 1) {
+            console.log("There is a black checker at the white home")
+            selectedChecker.isKing = true;
+            
+        }
+        
+        if(selectedChecker.color == `white` && selectedChecker.row == 8) {
+            console.log("There is a white checker at the black home")
+            selectedChecker.isWhiteKing = true;
+            
+        }
+        
+        selectedChecker = undefined
+        renderCheckers()
+    } else {
+        console.log(`select a checker, foo!`)
+    }
+}
+
+function clearBoard() {
+    $(`.black.cell`).html(``)
+    $(`black.cell`).unbind('click')
+    $(`.out-of-play`).html(``)
+}
